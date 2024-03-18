d75b00072a0bf89d3a80e9ec4802794396e14812
Ahmed Hawater <hawatercoding@gmail.com>
2024-03-18 21:14:20 +0200

    Fix unclosed reader problem in MetaDataManger

--- src/main/java/helpers/MetaDataManger.java
+++ src/main/java/helpers/MetaDataManger.java
@@ -14,56 +14,56 @@ public class MetaDataManger {
 
     private static final MetaDataManger INSTANCE = new MetaDataManger();
     private final File META_DATA_FILE = new File(ConfigReader.getInstance().read("MetaDataFileName"));
-    private FileReader fileReader;
-    private FileWriter fileWriter;
-
 
     private MetaDataManger() {
-        try {
-            fileReader = new FileReader(META_DATA_FILE);
-            fileWriter = new FileWriter(META_DATA_FILE);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
+
     public static MetaDataManger getInstance() {
         return INSTANCE;
     }
 
     public void writeAll(List<String[]> allLines) throws IOException {
+        FileWriter fileWriter = new FileWriter(META_DATA_FILE);
         CSVWriter writer = new CSVWriter(fileWriter);
         writer.writeAll(allLines);
         writer.close();
     }
 
-    public CSVReader getCSVReader() {
+    // WARNING! The reader should be closed after use
+    public CSVReader getCSVReader() throws IOException {
+        FileReader fileReader = new FileReader(META_DATA_FILE);
         return new CSVReader(fileReader);
     }
 
 
     public boolean exists(MetaDataColumns column, String value) throws IOException {
+        boolean exists = false;
         CSVReader csvReader = getCSVReader();
         String[] nextRecord;
         while ((nextRecord = csvReader.readNext()) != null) {
             if (nextRecord[column.ordinal()].equals(value)) {
-                return true;
+                exists = true;
+                break;
             }
         }
-        return false;
+        csvReader.close();
+        return exists;
     }
 
     public boolean existsInTable(MetaDataColumns column, String value, String tableName) throws IOException {
+        boolean exists = false;
         CSVReader csvReader = getCSVReader();
         String[] nextRecord;
         while ((nextRecord = csvReader.readNext()) != null) {
-            if (nextRecord[0].equals(tableName) && nextRecord[column.ordinal()].equals(value)) {
-                return true;
+            if (nextRecord[MetaDataColumns.TABLE_NAME.ordinal()].equals(tableName) && nextRecord[column.ordinal()].equals(value)) {
+                exists = true;
+                break;
             }
         }
-        return false;
+        csvReader.close();
+        return exists;
     }
-
 
 
 }
